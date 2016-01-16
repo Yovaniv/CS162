af64e5d09fc9f2655038a75230c965f7c50a89f7
joestrange8 <joestrange8@teknik.io>
2016-01-16 12:49:07 -0800

    Worked on the function that rotates the tetris piece

--- ArrayFun/parrish.cpp
+++ ArrayFun/parrish.cpp
@@ -12,9 +12,9 @@ void rotateRight(const bool tetrimino[MAX_ROWS][MAX_COLS]);
 int main (void){
 	bool tetrimino1[MAX_ROWS][MAX_COLS] = {
 		{false,false,false,false},
-		{false,false,false,false},
-		{false,false,false,true},
-		{true,true,true,true}};
+		{false,true,false,false},
+		{false,true,true,true},
+		{false,false,false,false}};
 	
 	bool tetrimino2[MAX_ROWS][MAX_COLS] = {
 		{false,false,false,false},
@@ -35,22 +35,9 @@ int main (void){
 	cout << "1st Array:" << endl;
 	dumpData(tetrimino1);
 	cout << "rotated 90 degrees to the right:" << endl;
+	rotateRight(tetrimino1);
 	cout << "rotated 90 degrees to the Left:" << endl;
-
-	cout << "2nd Array:" << endl;
-	dumpData(tetrimino2);
-	cout << "rotated 90 degrees to the right:" << endl;
-	cout << "rotated 90 degrees to the Left:" << endl;
-
-	cout << "3rd Array:" << endl;
-	dumpData(tetrimino3);
-	cout << "rotated 90 degrees to the right:" << endl;
-	cout << "rotated 90 degrees to the Left:" << endl;
-
-	cout << "4th Array:" << endl;
-	dumpData(tetrimino4);
-	cout << "rotated 90 degrees to the right:" << endl;
-	cout << "rotated 90 degrees to the Left:" << endl;
+	rotateLeft(tetrimino1);
 }
 
 void dumpData(const bool tetrimino[MAX_ROWS][MAX_COLS]){
@@ -67,10 +54,30 @@ void dumpData(const bool tetrimino[MAX_ROWS][MAX_COLS]){
 }
 
 
-void rotateLeft(const bool tetrimino[MAX_ROWS][MAX_COLS]){
-	
+void rotateRight(const bool tetrimino[MAX_ROWS][MAX_COLS]){
+	bool holder[MAX_ROWS][MAX_COLS];
+	for (int row = 3; row >= 0; row--){
+		for (int col = 0; col < MAX_COLS; col++){
+			if (tetrimino[row][col]==true){
+				holder [col][3-row] = true;
+			}else{
+				holder [col][3-row] = false;
+			}
+		}
+	}
+	dumpData(holder);
 }
 
-void rotateRight(const bool tetrimino[MAX_ROWS][MAX_COLS]){
-
+void rotateLeft(const bool tetrimino[MAX_ROWS][MAX_COLS]){
+	bool holder[MAX_ROWS][MAX_COLS];
+	for (int row = 0; row < MAX_ROWS; row++){
+		for (int col = 3; col >= 0; col--){
+			if (tetrimino[row][col]==true){
+				holder [3-col][row] = true;
+			}else{
+				holder [3-col][row] = false;
+			}
+		}
+	}
+	dumpData(holder);
 }
